3d110361f98847bf0d7fd49efbf9da4bd3d15f78
Reme Le Hane <reme.lehane@gmail.com>
2018-03-26 10:31:36 +0200

    fix: updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,13 @@ const webpack = require('webpack');
 
 const path = require('path');
 
-const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
-
 module.exports = {
   entry: {
     app: './src/index.js',
   },
 
   resolve: {
-    extensions: ['.jsx', '.js'],
-  },
-
-  externals: {
-    react: 'react',
-    'react-dom': 'react-dom',
+    extensions: ['.js'],
   },
 
   module: {
@@ -30,24 +23,12 @@ module.exports = {
 
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false },
-      comments: false,
-      sourceMap: false,
-      minimize: true,
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        ENV: JSON.stringify(ENV),
-        NODE_ENV: JSON.stringify(ENV),
-      },
-    }),
   ],
 
   output: {
     path: path.join(__dirname, '/build/'),
     filename: 'index.js',
-    library: 'redux-forms-markdown-editor',
+    library: 'cache-fetch',
     libraryTarget: 'umd',
   },
 };
